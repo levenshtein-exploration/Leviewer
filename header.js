29ba7f22a8e902f1764bcb60501797de9312947c
Yoann Dufresne <yoann.dufresne0@gmail.com>
2019-07-31 10:24:45 +0200

    select with correct data

--- header.js
+++ header.js
@@ -1,13 +1,22 @@
 
 var setup_header = function () {
   // Get the select to update
-  var header = document.getElementById("header");
+  var header = document.getElementById("experiment");
 
   // Request the file list file from the server
   fetch("/data/file_list.txt")
   .then(data=>{return data.text();})
   // On data, split the response into multiple options in the select.
-  .then(res=>{console.log(res, "TODO");});
+  .then(res=>{
+    console.log(res, "TODO");
+    res = res.split("\n");
+    for (let line of res) {
+      line = line.trim();
+      if (line.length > 0) {
+        header.innerHTML += "<option value='" + line + "'>" + line + "</option>";
+      }
+    }
+  });
 };
 
 setup_header();
