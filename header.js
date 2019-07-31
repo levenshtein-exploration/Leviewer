717c5bc2fe1faf0068016a9ad0e8e53e6c1a947e
Yoann Dufresne <yoann.dufresne0@gmail.com>
2019-07-31 11:23:58 +0200

    data_loaded event created

--- header.js
+++ header.js
@@ -1,22 +1,49 @@
 
-var setup_header = function () {
+var setup_selector = function () {
   // Get the select to update
-  var header = document.getElementById("experiment");
+  var selector = document.getElementById("experiment");
 
+  // --- Fill with correct values ---
   // Request the file list file from the server
   fetch("/data/file_list.txt")
   .then(data=>{return data.text();})
   // On data, split the response into multiple options in the select.
   .then(res=>{
-    console.log(res, "TODO");
     res = res.split("\n");
     for (let line of res) {
       line = line.trim();
       if (line.length > 0) {
-        header.innerHTML += "<option value='" + line + "'>" + line + "</option>";
+        selector.innerHTML += "<option value='" + line + "'>" + line + "</option>";
       }
     }
   });
+
+  // --- Setup the onclick event ---
+  selector.onchange = () => {
+    let value = selector.value;
+
+    // Do nothing for empty values
+    if (value.length == 0)
+      return;
+
+    let _DATA_LOADED = [];
+
+    // Load the corresponding file
+    fetch("/data/" + value).then(data=>{return data.text()}).then(res=>{
+      res = res.split("\n");
+      for (let line of res) {
+        if (line.length == 0)
+          continue;
+        _DATA_LOADED.push(line.split("\t"));
+      }
+
+      // Send an event to say that the data are loaded
+      var event = new Event("data_loaded");
+      event.data = _DATA_LOADED;
+      selector.dispatchEvent(event);
+    })
+  }
 };
 
-setup_header();
+setup_selector();
+
